731afe68f8830b6a4b377122d43b00691bf3e246
Kim Do <64578699+lilkimchi@users.noreply.github.com>
2020-12-01 21:54:44 +0100

    Add files via upload

---
+++ get_comments_from_video.py
@@ -0,0 +1,76 @@
+
+from apiclient.discovery import build
+#from apiclient.errors import HttpError
+#from oauth2client.tools import argparser # removed by Dongho
+import argparse
+import csv
+import unidecode
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyBugiNIjhyaLI8gmZNnRPOo-y_qyP6weTk"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def get_video_comments(service, **kwargs):
+    comments = []
+    results = service.commentThreads().list(**kwargs).execute()
+
+    while results:
+        for item in results['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments.append(comment)
+
+        # Check if another page exists
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = service.commentThreads().list(**kwargs).execute()
+        else:
+            break
+
+    return comments
+
+
+def write_to_csv(comments):
+    with open('comments.csv', 'w') as comments_file:
+        comments_writer = csv.writer(comments_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        comments_writer.writerow(['Video ID', 'Title', 'Comment'])
+        for row in comments:
+            # convert the tuple to a list and write to the output file
+            comments_writer.writerow(list(row))
+
+
+def get_videos(service, **kwargs):
+    final_results = []
+    results = service.search().list(**kwargs).execute()
+
+    i = 0
+    max_pages = 3
+    while results and i < max_pages:
+        final_results.extend(results['items'])
+
+        # Check if another page exists
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = service.search().list(**kwargs).execute()
+            i += 1
+        else:
+            break
+
+    return final_results
+
+
+def search_videos_by_keyword(service, **kwargs):
+    results = get_videos(service, **kwargs)
+    final_result = []
+    for item in results:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        comments = get_video_comments(service, part='snippet', videoId=video_id, textFormat='plainText')
+        # make a tuple consisting of the video id, title, comment and add the result to
+        # the final list
+        final_result.extend([(video_id, title, comment) for comment in comments])
+
+    write_to_csv(final_result)
